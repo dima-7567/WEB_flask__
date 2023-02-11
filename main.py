e8b4baa97d73ec7bf6c934a70964fa7ed8273a88
Student <dima.tsygankov.2006@mail.ru>
2023-02-11 19:03:54 +0300

    training

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    with open('static/html_/main.html') as main_html:
-        return main_html.read()
+    with open('templates/html_/base.html') as main_html:
+        return '<a href=http://127.0.0.1:12398/index>to go </a>'
 
 
-@app.route('/index')
-def index():
-    with open('static/html_/index.html', encoding='UTF-8') as index_html:
-        return index_html.read()
+@app.route('/index/<page_name>')
+def index(page_name):
+    if 'инженер' in page_name or 'строител' in page_name:
+        return render_template('html_/base.html', page_name=page_name,
+                               src='/static/Inkeda23bd91402c815b048859dca0c8cc266_LI.jpg')
+    else:
+        return render_template('html_/base.html', page_name=page_name,
+                               src='/static/a23bd91402c815b048859dca0c8cc266.jpg')
 
 
 if __name__ == '__main__':
